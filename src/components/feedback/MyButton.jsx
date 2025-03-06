1cf9046ee48c6b306a2aa6b5b9e973743fede7e3
Zichler <nikcih99@gmail.com>
2025-03-06 01:21:24 +0100

    Updates

--- src/components/feedback/MyButton.jsx
+++
@@ -1,5 +0,0 @@
-import "./myButton.css"
-
-export default function MyButton({text, func}) {
-  return  <button onClick={func} className='myButton'>{text}</button>;
-}
--- src/components/loader/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./loader.modules.css"
-
-export default function Loader() {
-  return (
-    <div className={styles.loader}></div>
-  )
-}
